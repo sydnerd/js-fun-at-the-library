c84d3e59442efb83512d29852d2dcffaf3391023
Sydney Bear <sbear3915@gmail.com>
2021-03-24 21:37:59 -0600

    Complete saveReview and buildMainCharacter and createTitle

--- src/book.js
+++ src/book.js
@@ -1,12 +1,32 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return `The ${bookTitle}`;
 }
+
+function buildMainCharacter(name,age,pronouns){
+  var characterObject = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return characterObject;
+}
+
+function saveReview(reviewName,reviewList){
+  if (!reviewList.includes(reviewName)){
+    reviewList.push(reviewName);
+  }
+}
+
+function calculatePageCount(){}
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
 }
+// buildMainCharacter,
+// saveReview,
+// calculatePageCount,
+// writeBook,
+// editBook
