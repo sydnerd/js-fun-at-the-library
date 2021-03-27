20e76b422fea2509854e39e2f4de3375dde68542
Sydney Bear <sbear3915@gmail.com>
2021-03-27 15:28:35 -0600

    Add createLibrary function

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,25 @@
+function createLibrary(name, shelves){
+  var library ={
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    },
+  };
+  return library
+};
+
+function addBook(library,book){
+  var bookAdded =[];
+  bookAdded.pop(library.shelves.fantasy[0]);
+};
+return bookAdded
 
 
 module.exports = {
-  // createLibrary,
+  createLibrary,
+  addBook,// cre,ateLibrary,
   // addBook,
   // checkoutBook
 };
