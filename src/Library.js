8eaa8692c830ce7aa54a18edadf20739fbe17fcb
Sydney Bear <sbear3915@gmail.com>
2021-03-27 17:54:04 -0600

    Update checkoutBook function

--- src/Library.js
+++ src/Library.js
@@ -10,14 +10,25 @@ function createLibrary(name, shelves){
   return library
 };
 
-function addBook(library,book){
+function addBook(library,book) {
   library.shelves[book.genre].push(book);
 };
+
+function checkoutBook(library, title, genre) {
+  for(var i =0; i < library.shelves[genre].length; i++){
+    var bookName =library.shelves[genre][i];
+    if(bookName.title === title){
+      library.shelves[genre].splice(genre[i],1);
+      return `You have now checked out ${title} from the ${library.name}`;
+    };
+  }; return `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+
+  };
+
 
 
 module.exports = {
   createLibrary,
-  addBook,// cre,ateLibrary,
-  // addBook,
-  // checkoutBook
-};
+  addBook,
+  checkoutBook,
+}
