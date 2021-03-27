5ba5fdd447834368fc0642a496581b287006246a
Sydney Bear <sbear3915@gmail.com>
2021-03-26 18:19:50 -0600

    Add searchShelf

--- src/shelf.js
+++ src/shelf.js
@@ -13,11 +13,30 @@ function unshelfBook(book, shelf) {
   }
 }
 
+function listTitles(shelf) {
+  var titleList = []
+  for(var i =0; i< shelf.length; i++){
+    titleList.push(shelf[i].title);
+  }
+    return titleList.join(", ");
+}
+
+function searchShelf(shelf,isTrue) {
+  var titles = [];
+  for(var i=0; i< shelf.length; i++){
+      titles.push(shelf[i].title);
+  }
+  return titles.includes(isTrue)
+  //check if it's true
+}
+
 
 
 module.exports = {
   shelfBook,
   unshelfBook,
+  listTitles,
+  searchShelf,
   // shelfBook,
   // unshelfBook,
   // listTitles,
--- test/shelf-test.js
+++ test/shelf-test.js
@@ -169,11 +169,11 @@ describe("shelf.js", function() {
   });
 
   describe("listTitles", function () {
-    it.skip("should be a function", function () {
+    it("should be a function", function () {
       assert.isFunction(listTitles);
     });
 
-    it.skip("should create a list of all the titles on a shelf", function () {
+    it("should create a list of all the titles on a shelf", function () {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -201,7 +201,7 @@ describe("shelf.js", function() {
   });
 
   describe("searchShelf", function() {
-    it.skip("should tell us if a title is on the shelf", function() {
+    it("should tell us if a title is on the shelf", function() {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -220,7 +220,7 @@ describe("shelf.js", function() {
       assert.equal(searchShelf(sciFiShelf, "The Fifth Season"), false);
     });
 
-    it.skip("should tell us if a title is on a different shelf", function() {
+    it("should tell us if a title is on a different shelf", function() {
       var bossypants = {
         title: "Bossypants",
         mainCharacter: { name: "Tina Fey", age: 50, pronouns: "she/her" },
